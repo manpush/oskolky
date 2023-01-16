b5271a50d49f8e09e7a63cd451747f4aef1ff7e9
Max <5646055@mail.ru>
2023-01-16 11:35:57 +0300

    mini edits

--- qwe2.py
+++
@@ -1,8 +0,0 @@
-count = int(input("count people:"))
-y, n = 0, 0
-for i in range(count):
-    if int(input("1 or 2 or 3")) == 2:
-        n += 1
-    else:
-        y += 1
-print(int(y/(y+n) * 100))
