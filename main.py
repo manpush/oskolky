984d5822587729d4ef5d0ba498e9c09eeaa7b8b5
Max <5646055@mail.ru>
2023-01-17 13:52:16 +0300

    мелкие правки, наведение красоты

--- main.py
+++ main.py
@@ -31,38 +31,44 @@ squere_count = [
 ]
 
 
-class event_reaction:
-    def __init__(self, mass_react, keff, air_resistance):
+class EventReaction:
+    def __init__(self, mass_react, keff):
         self.mass_react, self.keff = mass_react, keff
 
 
-class glass:
+def get_count_parts(x, y):
+    return math.sqrt(x * y) / 100
+
+
+class Glass:
     # m - масса вв, r - расстояние от места взрыва, ff - прочность на растяжение, a -коэффициент эквивалентности ВВ
     # ae - – доля энергии взрыва, ii - Импульс фазы сжатия, rr - приведенное расстояние
     # ee - поправочный коэффициент, p - плотность стекла,h – толщина стекла,eo – модуль Юнга
     # delp– давление во фронте,v0 - Скорость осколка,q– эффективная масса взрывчатого вещества
     # fad - f_d*ε_d
-    def __init__(self, ff, eo, ee, p, h, distance_x, pos_dh, distance_z, air_resistance, parent, size_x, size_y):
+    def __init__(self, ff, eo, ee, density, h, distance_x, pos_dh, distance_z, air_resistance, parent, size_x, size_y):
         """
         :param ff: прочность на растяжение
         :param eo: модуль Юнга
         :param ee: поправочный коэффициент
-        :param p: плотность стекла
+        :param density: плотность стекла
         :param h: толщина стекла
         :param distance_x: расстояние от места взрыва
         :param pos_dh: высота стекла над уровнем взрыва
         :param distance_z: смещение в сторону от взрыва
-        :param air_resistance: Сопротивление воздуха
+        :param air_resistance: ??Сопротивление??
         :param parent: event info
-        :param square: площадь стекла
+        :param size_x: ширина стекла
+        :param size_y: высота стекла
         """
-        self.ff, self.eo, self.ee, self.p, self.h, self.distance_x, self.pos_dh, self.distance_z, self.air_resistance = ff, eo, ee, p, h, distance_x, pos_dh, distance_z, air_resistance
+        self.ff, self.eo, self.ee, self.p, self.h, self.distance_x, self.pos_dh, self.distance_z, self.air_resistance \
+            = ff, eo, ee, density, h, distance_x, pos_dh, distance_z, air_resistance
         self.parent = parent
         self.size_x = size_x
         self.size_y = size_y
         self.mass_react = parent.mass_react
 
-    def projectile(self, cor_dh=0, cor_left=0):
+    def projectile(self, cor_dh: float = 0, correct_left: float = 0):
         q = (1 - fractionOfExplosionEnergy) * equivalenceCoefficientVV * self.parent.mass_react  # q –
         # эффективная масса взрывчатого вещества
         rr = math.sqrt(self.distance_x ** 2 + self.distance_z ** 2) / q ** (1 / 3)  # rr - приведенное расстояние
@@ -76,17 +82,17 @@ class glass:
         theta = math.atan(self.pos_dh + cor_dh / math.sqrt(self.distance_x ** 2 + self.distance_z ** 2))
         g = 9.81
         m = self.size_x * self.size_y * self.h / 100 * self.p
-        S = self.size_x * self.size_y
-        time = np.linspace(0, 100, 3000)
+        square = self.size_x * self.size_y
+        time = np.linspace(0, 100, 10000)
         tof = 0.0
-        alpha = math.atan(self.distance_z + cor_left / math.sqrt(self.distance_x ** 2 + cor_dh ** 2))
+        alpha = math.atan((self.distance_z + correct_left) / math.sqrt(self.distance_x ** 2 + cor_dh ** 2))
         dt = time[1] - time[0]
         gravity = -g * m
         v_x = v * math.fabs(np.cos(theta))
         v_y = v * np.sin(theta)
         r_x = 0
         r_y = dh + cor_dh
-        r_z = cor_left
+        r_z = correct_left
 
         r_xs = list()
         r_ys = list()
@@ -97,18 +103,18 @@ class glass:
         r_zs.append(r_z)
 
         for t in time:
-            F_x = 0.0
-            F_y = 0.0
-            dd = 0.5 * self.air_resistance * S / m
-            F_y = F_y - dd
-            F_x = F_x - dd * np.sign(v_x)
-            F_y = F_y + gravity
+            f_x = 0.0
+            f_y = 0.0
+            dd = 0.5 * self.air_resistance * square / m
+            f_y = f_y - dd
+            f_x = f_x - dd * np.sign(v_x)
+            f_y = f_y + gravity
 
-            r_x = (r_x + v_x * dt * np.cos(alpha) + (F_x / (2 * m)) * dt ** 2)
-            r_y = (r_y + v_y * dt + (F_y / (2 * m)) * dt ** 2)
-            r_z = cor_left + np.tan(alpha) * r_x
-            v_x = v_x + (F_x / m) * dt
-            v_y = v_y + (F_y / m) * dt
+            r_x = (r_x + v_x * dt * np.cos(alpha) + (f_x / (2 * m)) * dt ** 2)
+            r_y = (r_y + v_y * dt + (f_y / (2 * m)) * dt ** 2)
+            r_z = correct_left + np.tan(alpha) * r_x
+            v_x = v_x + (f_x / m) * dt
+            v_y = v_y + (f_y / m) * dt
 
             r_xs.append(r_x)
             r_ys.append(r_y)
@@ -119,25 +125,21 @@ class glass:
 
         return r_xs, r_ys, r_zs, tof
 
-    def get_count_parts(self, x, y):
-        return math.sqrt(x * y) / 100
-
-    def print_destroy(self, ax):
-        partsCount = self.get_count_parts(self.size_x, self.size_y)
+    def print_destroy(self, axes):
+        parts_count = get_count_parts(self.size_x, self.size_y)
         for i in squere_count:
-            for j in range(int(i[1] * partsCount)):
-                cor_dh = random.uniform(0, int(self.size_x / 100))
-                cor_left = random.uniform(0, int(self.size_y / 100))
-                r_xs, r_ys, r_zs, tof = glass(self.ff, self.eo, self.ee, self.p, self.h, self.distance_x, self.pos_dh,
+            for j in range(int(i[1] * parts_count)):
+                cor_dh = float(random.uniform(0, int(self.size_x / 100)))
+                corect_left = float(random.uniform(0, int(self.size_y / 100)))
+                r_xs, r_ys, r_zs, tof = Glass(self.ff, self.eo, self.ee, self.p, self.h, self.distance_x, self.pos_dh,
                                               self.distance_z,
                                               random.uniform(0.2, 1.2) / 10, self, math.sqrt(i[0]),
-                                              math.sqrt(i[0])).projectile(cor_dh, cor_left)
-                ax.plot(r_xs, r_zs, r_ys)
+                                              math.sqrt(i[0])).projectile(cor_dh, corect_left)
+                axes.plot(r_xs, r_zs, r_ys)
 
 
-er = event_reaction(massVV, equivalenceCoefficientVV, 1.225)
-gss = glass(tensileStrength, moduleUng, correctionFactor, p, depth, lenToBomb, dh, cor_left, 0.2, er, y_size, x_size)
-
+er = EventReaction(massVV, equivalenceCoefficientVV)
+gss = Glass(tensileStrength, moduleUng, correctionFactor, p, depth, lenToBomb, dh, 10, 0.2, er, y_size, x_size)
 fig = plt.figure(figsize=(6, 6))
 ax = fig.add_subplot(111, projection='3d')
 gss.print_destroy(ax)
